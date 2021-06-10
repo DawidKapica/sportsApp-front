fddb0e3e2a7ab426a233adeda4c93148124c8052
Dawid Kapica <kadaw77@gmail.com>
2021-06-11 00:35:24 +0200

    Final v4.0.4 Repair problems with nulls

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static('/'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/login'));
+    res.sendFile(path.join('/login'));
 });
 
 // Start the app by listening on the default Heroku port
