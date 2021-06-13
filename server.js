da81c73df5610e39d8fcebf208de81e1411f305e
Dawid Kapica <kadaw77@gmail.com>
2021-06-13 03:01:33 +0200

    Final v4.0.4 Repair problems with nulls

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/'));
+//
+// app.get('/*', function(req,res) {
+//
+//     res.sendFile(path.join(__dirname+'/src/index.html'));
+// });
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +22,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sports-app-front'));
+app.use(express.static(__dirname + '/dist/sportsApp-front'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/src/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/sportsApp-front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
